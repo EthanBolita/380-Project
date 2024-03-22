3fcea967b6d3b97038c70c6cdb6d10045b24b3ee
Ethan Bolita <149443930+EthanBolita@users.noreply.github.com>
2024-03-21 19:09:16 -0700

    Add files via upload

---
+++ code.java
@@ -0,0 +1,56 @@
+class Test {
+   public static void main(String[] args) {
+    System.out.println("Hello World");
+   } 
+    
+}
+public class shoppingCart{
+   string product;
+   double productCost;
+   int quantity;
+   public shoppingCart(String product, double productCost, int quantity){
+this.product=product;
+this.productCost=product;
+this.quantity=quantity;
+   }
+}
+private class creditCard{
+   string customerName;
+   string cardNumber;
+   int CCV;
+   string expDate;
+   string address;
+   string email;
+
+
+  /*setEmail;
+   setAddress;
+   setName;
+   setCarrinfo;*/
+}
+
+public class manager{
+   //inventory;
+   string managerName;
+   string managerAddress;
+   string login;
+   string password;
+
+
+}
+public class inventory{
+   char shirtColor;
+   char seatscolor;
+   char sweaterColor;
+   static int numOfShirts;
+   static int numOfSweats;
+   static int numOfSweaters;
+public static void main(String[] args) {
+   int a = numOfShirts;
+   int b = numOfSweats;
+   int c = numOfSweaters;
+   System.out.println("The total number of products is " + Integer.sum(a, b, c));
+}
+   
+   
+}

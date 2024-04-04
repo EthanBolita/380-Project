82970ba05e6612d74ae006bb02e282d0aa360ac8
Ethan Bolita <149443930+EthanBolita@users.noreply.github.com>
2024-04-03 18:31:44 -0700

    Create manager.java

---
+++ manager.java
@@ -0,0 +1,15 @@
+public class manager{
+   String managerName;
+   String managerAddress;
+   String login;
+   String password;
+   
+
+
+}
+public static void main(String[] args){
+    myInventory =new Inventory();
+    myInventory.add(0);
+    myInventory.remove(0);
+
+}

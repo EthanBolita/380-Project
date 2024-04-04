240522a38701100254e6959f6ac7afd4feef843b
Ethan Bolita <149443930+EthanBolita@users.noreply.github.com>
2024-04-03 18:35:06 -0700

    search.java

---
+++ search.java
@@ -0,0 +1,51 @@
+public class search{
+    int  productID;
+    char size;
+    String style;
+    String color;
+    double price;
+
+
+    public search(int  productID, char size, String style, String color, double price ){
+        this.productID= productID;
+        this.size=size;
+        this.style=style;
+        this.color=color;
+        this.price=price;
+    }
+public int getProductID(){
+    return productID;
+}
+public char getSize(){
+    return size;
+}
+
+public String getStyle(){
+    return style;
+}
+
+public String getColor(){
+    return color;
+}
+
+public double getPrice(){
+    return price;
+}
+
+public void setProductID(){
+    this.productID=productID;
+}
+public void setSize(){
+    this.size=size;
+}
+public void setStyle(){
+    this.style=style;
+}
+public void setColor(){
+    this.color=color:
+}
+public void setPrice(){
+    this.price=price;
+}
+
+}

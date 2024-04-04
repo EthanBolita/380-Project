57fdc2d1a7aa8b2b2e488ad8a8e8f90c6b209276
Ethan Bolita <149443930+EthanBolita@users.noreply.github.com>
2024-04-03 18:30:18 -0700

    customer.java

---
+++ customer.java
@@ -0,0 +1,26 @@
+public class cutomer{
+    String name;
+   String address;
+   String card;
+   String email;
+
+public void setName(name){
+    this.name=name;
+}
+public void setAddress(address){
+    this.address=address;
+}
+public void setCard(card){
+    this.card=card;
+}
+public void setEmail(email){
+    this.email=email;
+}
+
+
+
+public static void main(String[] args){
+    myShoppingCart= new myShoppingCart();
+}
+
+}

419ab2e10970e5991c5ca048718edb8dcb0e4df2
Michael-S-Powell <michael.powell.108@my.csun.edu>
2024-04-09 15:07:38 -0700

    new classes
    created different classes for card, inventory, and product

---
+++ product.java
@@ -0,0 +1,33 @@
+public class product{
+   int id;
+   char color;
+   int quanitiy;
+   double price;
+   char size;
+   String type;
+  
+  public double getPrice(){
+    return price;
+  }
+
+  public char getColor(){
+    return color;
+  }
+
+  public int getQuanitiy(){
+    return quanitiy;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public char getSize(){
+    return size;
+  }
+
+}
--- shoppingCart.java
+++ shoppingCart.java
@@ -1,106 +1,12 @@
-class Test {
-   public static void main(String[] args) {
-    System.out.println("Hello World");
-   } 
-    
-}
 public class shoppingCart{
-ArrayList<product> products= new ArrayList<product>();
-   double productCost;
-   double totalCost;
-   public shoppingCart(String product, double productCost, int quantity){
-this.product=product;
-this.productCost=productCost;
-this.quantity=quantity;
-   }
-}
-private class creditCard{
-   String customerName;
-   String cardNumber;
-   String CCV;
-   int month;
-   int year;
-  
-  if (cardNumber.matches ("[0-9]+") && cardNumber.length == 11){
-    cardNumber=cardNumber;
-  }else{
-      System.out.println("Invalid. Try again");
+  ArrayList<product> products= new ArrayList<product>();
+  double productCost;
+  double totalCost;
+  public shoppingCart(String product, double productCost, int quantity){
+    this.product=product;
+    this.productCost=productCost;
+    this.quantity=quantity;
   }
-  if (customerName.matches("[a-zA-Z]+")){
-  customerName=customerName;
-  }else{
-      System.out.println("Invalid. Try again");
-  }
-   if (CCV.matches ("[0-9]+") && CCV.length == 2){
-  CCV=CCV;
-  }else{
-      System.out.println("Invalid. Try again");
-  }
-  if (month > 0 && month < 13){
-      month=month;
-      }else{
-          System.out.println("Invalid. Try again");
-      }
-}
-
-public class manager{
-   String managerName;
-   String managerAddress;
-   String login;
-   String password;
-   Inventory myInventory=new Inventory();
-
-
-}
-public class product{
-   int id;
-   char color;
-   int quanitiy;
-   double price;
-   char size;
-   String type;
-  
-  public double getPrice(){
-    return price;
-  }
-
-  public char getColor(){
-    return color;
-  }
-
-  public int getQuanitiy(){
-    return quanitiy;
-  }
-
-  public String getType(){
-    return type;
-  }
-
-  public int getId(){
-    return id;
-  }
-
-  public char getSize(){
-    return size;
-  }
-
-}
-   
-   
-
-public class Inventory{
- ArrayList<product> sweats= new ArrayList<product>();
- ArrayList<product> sweaters= new ArrayList<product>();
- ArrayList<product> shirts= new ArrayList<product>();
-}
- 
- 
- 
- 
- 
   public static void main(String[] args) {
-   int a = numOfShirts;
-   int b = numOfSweats;
-   int c = numOfSweaters;
-   System.out.println("The total number of products is " + Integer.sum(a, b, c)); 
   } 
+}

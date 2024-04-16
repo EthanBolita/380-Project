fea8f48557990cfd365d6c091d718b56b971bd95
Michael-S-Powell <michael.powell.108@my.csun.edu>
2024-04-16 15:11:55 -0700

    shoppingCart
    printContent for shoppingCart class: prints all products in cart

--- shoppingCart.java
+++ shoppingCart.java
@@ -19,8 +19,9 @@ public class shoppingCart{
   public void remove(product item){
     for(int i=0; i<products.size();i++){
       if(products.get(i).getId()==item.getId()){
+        totalCost-=products.get(i).getPrice();
         products.remove(i);
-        totalCost-=item.getPrice();
+        totalCost= (double) Math.round(totalCost * 100) / 100;
       }
     }
   }
@@ -29,14 +30,26 @@ public class shoppingCart{
       return totalCost;
   }
 
+  public String printContent(){
+    String value="";
+    for(int i=0; i<products.size();i++){
+      value+=products.get(i).toString();
+      if(i!=products.size()-1){
+        value+="\n";
+      }
+    }
+    return value;
+  }
+
     
   public static void main(String[] args) {
     shoppingCart temp=new shoppingCart();
     product item=new product(0,"red",19.99, "small", "shirt");
-    product item2=new product(0,"black",24.99, "small", "pants");
+    product item2=new product(1,"black",24.99, "small", "pants");
     temp.addToCart(item);
     temp.addToCart(item2);
     System.out.println(temp.getTotal());
+    System.out.println(temp.printContent());
     temp.remove(item);
     System.out.println(temp.getTotal());
   } 
